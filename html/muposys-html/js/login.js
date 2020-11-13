d4d2ec59ae8e998de682d67fb3eb084b74d7eaf9
Azael Reyes <azael.devel@gmail.com>
2020-11-13 00:12:01 -0700

    Working on create user

--- html/muposys-html/js/login.js
+++ html/muposys-html/js/login.js
@@ -1,4 +1,4 @@
 window.onload = function display_login() {
-  var modal = document.getElementById('loginForm');
+  var modal = document.getElementById('formModal');
 	modal.style.display = "block";
 }
