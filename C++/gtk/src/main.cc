496644baffe1ad5924f66df6dd5cf83f2daa6548
Azael Reyes <azael.devel@gmail.com>
2022-03-25 10:28:09 -0700

    save

--- C++/gtk/src/main.cc
+++ C++/gtk/src/main.cc
@@ -1,13 +1,14 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-  */
 /*
  * main.cc
- * Copyright (C) 2020 Azael R. <azael.devel@gmail.com>
+ * Copyright (C) 2022 Azael R. <azael.devel@gmail.com>
  * 
- * sysapp is free software: you can redistribute it and/or modify it
+ * muposys is free software: you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by the
  * Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  * 
- * sysapp is distributed in the hope that it will be useful, but
+ * muposys is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
  * See the GNU General Public License for more details.
@@ -29,15 +30,12 @@
 
 
 /* For testing propose use the local (not installed) ui file */
-/* #define UI_FILE PACKAGE_DATA_DIR"/ui/sysapp.ui" */
+/* #define UI_FILE PACKAGE_DATA_DIR"/ui/muposys.ui" */
 #define UI_FILE "src/muposys.ui"
 
-
-//#include "Login.hh"
-#include "Main.hh"
-
-
-int main (int argc, char *argv[])
+   
+int
+main (int argc, char *argv[])
 {
 	Gtk::Main kit(argc, argv);
 
@@ -53,16 +51,14 @@ int main (int argc, char *argv[])
 		std::cerr << ex.what() << std::endl;
 		return 1;
 	}
-	
-	//Login* wndLogin = 0;
-	muposys::Main* wndMain = 0;
-	builder->get_widget_derived("wndMain", wndMain);
+	Gtk::Window* main_win = 0;
+	builder->get_widget("main_window", main_win);
 
-	if (wndMain)
+
+	if (main_win)
 	{
-		kit.run(*wndMain);
+		kit.run(*main_win);
 	}
-	
 	return 0;
 }
 
