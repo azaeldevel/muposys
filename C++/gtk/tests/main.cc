8b50a044b33d1abcf71fe80ce6574f6f9e0126df
Azael Reyes <azael.devel@gmail.com>
2022-03-25 10:40:02 -0700

    save

---
+++ C++/gtk/tests/main.cc
@@ -0,0 +1,29 @@
+
+
+#include "v0.hh"
+
+int main(int argc, char *argv[])
+{  
+
+	/* initialize the CUnit test registry */
+	if (CUE_SUCCESS != CU_initialize_registry()) return CU_get_error();
+	CU_pSuite pSuite = NULL;
+	pSuite = CU_add_suite("Testing muposys", init, clean);
+	if (NULL == pSuite)
+	{
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+	
+	if ((NULL == CU_add_test(pSuite, "Developing", v0_developing)))
+	{
+		CU_cleanup_registry();
+		return CU_get_error();
+	}
+	
+	/* Run all tests using the CUnit Basic interface */
+	CU_basic_set_mode(CU_BRM_VERBOSE);
+	CU_basic_run_tests();
+	CU_cleanup_registry();
+	return CU_get_error();	
+}
---
+++ C++/gtk/tests/v0.cc
@@ -0,0 +1,28 @@
+
+
+
+#include "v0.hh"
+
+using namespace oct::mont;
+
+int v0_init(void)
+{
+	if(not std::filesystem::exists("../../tests"))
+	{
+		std::cout << "No esta en el directorio correcto\n";
+		return 1;
+	}
+	
+	return 0;
+}
+
+int v0_clean(void)
+{
+	return 0;
+}
+
+void v0_developing()
+{
+	
+}
+
