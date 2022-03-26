309b74282b7aee72debe63e9aecbe0ee064e7db9
Azael Reyes <azael.devel@gmail.com>
2022-03-25 23:25:17 -0700

    save

--- C++/gtk/src/muposys.cc
+++ C++/gtk/src/muposys.cc
@@ -6,14 +6,14 @@ namespace mps
 
 Main::Main(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade) : Gtk::Window(cobject), builder(refGlade)
 {
-	set_title(_("Multi-Porpuse System"));
 	
 	bt_close = 0;
 	builder->get_widget("bt_close", bt_close);
 	bt_close->signal_clicked().connect(sigc::mem_fun(*this,&Main::on_bt_close_clicked));
 	
 	hb_muposys = 0;
-	builder->get_widget("hb_muposys", hb_muposys);
+	builder->get_widget("hb_muposys", hb_muposys);	
+	hb_muposys->set_subtitle(_("Multi-Porpuse System"));
 	
 }
 Main::~Main()
