3ec1eb19a0e5c34591eb45c0ad7a281cf1ddb8e2
Azael Reyes <azael.devel@gmail.com>
2022-09-08 23:49:51 -0700

    working on add permision module

--- tests/v0.cc
+++ tests/v0.cc
@@ -1,4 +1,7 @@
 
+#include <filesystem>
+#include <random>
+
 #include "v0.hh"
 
 
@@ -15,10 +18,15 @@ int v0_clean(void)
 
 void v0_apidb()
 {
+	std::random_device generator;
+  	std::uniform_int_distribution<int> randInt(1,INT_MAX);
+	
 	octetos::db::maria::Connector connmaria;
 	bool flag = connmaria.connect(muposysdb::datconex);
 	if(flag)
 	{		
+		CU_ASSERT(flag);
+		
 		//if(verbose) std::cout << "Listando los que tiene 8 con 5 registro maximo." << std::endl;
 		std::vector<muposysdb::Persons*>* lst = muposysdb::Persons::select(connmaria,"name1 like 'n1-%8'");
 		if(lst != NULL)
@@ -39,9 +47,37 @@ void v0_apidb()
 		delete lst;
 
 
-		//std::vector<muposysdb::Users*>* userlst = muposysdb::Users::select(connmaria,0,'D');
-	}
+		std::vector<muposysdb::Users*>* userlst = muposysdb::Users::select(connmaria,"",0,'D');
+		if(userlst != NULL)
+		{
+		    for(auto u : *userlst)
+		    {
+				if(u->getPerson().downName1(connmaria) and u->getPerson().downName3(connmaria)) 
+				{
+					CU_ASSERT(true);
+					//std::cout << u->getPerson().getName1() << " " << u->getPerson().getName3() << std::endl;
+				}
+		    }
+			for(auto u : *userlst)
+			{
+				delete u;
+			}
+		}
+		delete lst;
 
+		muposysdb::Permissions permss;
+		int randNumber = randInt(generator);
+		std::string name_perss = "permss-" + std::to_string (randNumber);
+		std::string brief_perss = "biref-" + std::to_string (randNumber);
+		CU_ASSERT(permss.insert(connmaria,name_perss,brief_perss));
+
+
+
+		
+		connmaria.commit();
+		connmaria.close();
+	}
+	
 	
 }
 
@@ -49,16 +85,17 @@ void v0_apidb()
 void v0_develop()
 {
 	CU_ASSERT(true);
+
+	std::string database_file = "http";
+
+	CU_ASSERT(std::filesystem::exists(database_file));
 	
-	std::cout << "database : " << muposys::http::db::database_file << "\n";
-
-
 	std::random_device rd;  //Will be used to obtain a seed for the random number engine
     std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
     std::uniform_int_distribution<> distrib(1, 1000000);
 	
-	Conector conn(muposys::http::db::database_file);
-	CU_ASSERT(conn.getServerConnector() != NULL)
+	Conector conn(database_file);
+	CU_ASSERT(conn.getServerConnector() != NULL);
 	Session session;	
 	std::string hoststr,sessionstr,timestr;
 	hoststr = std::to_string(distrib(gen));
