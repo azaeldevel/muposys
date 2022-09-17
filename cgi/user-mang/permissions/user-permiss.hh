34bf21fa9027dff811adbe0d472dd2d2eda773d2
Azael Reyes <azael.devel@gmail.com>
2022-09-16 22:30:42 -0700

    Working on add user - permission

--- cgi/user-mang/permissions/user-permiss.hh
+++ cgi/user-mang/permissions/user-permiss.hh
@@ -3,6 +3,8 @@
 #define MUPOSYS_USER_PERMISSIONS_HH
 
 
+#include <apidb/muposysdb.hpp>
+
 #include "../../application.hh"
 
 namespace muposys
@@ -10,12 +12,26 @@ namespace muposys
 
 class BodyUserPermission : public BodyApplication
 {
-private:	
 public:
 	BodyUserPermission();
+	~BodyUserPermission();
+
 
 	virtual void print(std::ostream& out) const;
 
+private:
+	std::vector<muposysdb::Users*>* userlst;
+	std::vector<muposysdb::Permissions*>* permisslst;
+	
+#if defined MARIADB
+	octetos::db::maria::Connector conn;
+#elif defined MYSQL
+	octetos::db::mysql::Connector conn;
+#elif defined POSTGRESQL
+	octetos::db::postgresql::Connector conn;
+#else
+	#error "Base de datos desconocida."
+#endif
 };
 
 class UserPermission : public Application
@@ -28,7 +44,8 @@ public:
 	
 private:	
 	cgicc::Cgicc formData;
-	bool cgi;
+
+
 };
 
 }
