dbe61758475851c61c68e9210888ea7eb8338c44
Azael Reyes <azael.devel@gmail.com>
2022-10-04 01:31:34 -0700

    save

--- desk/desk-main.cc
+++ desk/desk-main.cc
@@ -32,7 +32,7 @@ int main (int argc, char *argv[])
 #else
 		fileui = PACKAGE_DATA_DIR;
 #endif
-		fileui += "/desk.ui";
+		fileui += "/muposys-desk.ui";
 		builder->add_from_file(fileui);
 	}
 	catch (const Glib::FileError & ex)
