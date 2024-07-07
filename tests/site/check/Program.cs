ac693f472b3999534dd1b176e53b1fff2dee40e3
Azael <azael.devel@gmail.com>
2024-07-06 19:21:22 -0700

    save

---
+++ tests/site/check/Program.cs
@@ -0,0 +1,22 @@
+﻿
+string inputsparams = Console.ReadLine();
+string[] pars = inputsparams.Split('&');
+string[] pair;
+string key, value;
+Dictionary<string, string> POST = new Dictionary<string, string>();
+
+foreach (string str in pars)
+{
+    pair = str.Split('=');
+    if (pair.Length > 0) key = pair[0];
+    else key = "";
+    if (pair.Length > 1) value = pair[1];
+    else value = "";
+    POST.Add(key,value);
+}
+
+int counter = Int32.Parse(POST["counter"]);
+
+Console.Write("Content-type: text/html\n\n");
+Console.Write(counter + 1);
+
